478281c05a60c04669e2b4e5181231bd04fbf35b
byt23 <berkayyasar2004@gmail.com>
2026-04-16 11:17:25 +0300

    Initial Commit

---
+++ AuraHertz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuraHertz
+//
+//  Created by BERKAY TURAN on 16.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
